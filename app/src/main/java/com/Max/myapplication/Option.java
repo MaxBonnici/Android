8b840953f07d9beb0da1d5f61c95c607bd608195
AnisMouniym <95011291+AnisMouniym@users.noreply.github.com>
2023-02-08 16:30:11 +0100

    usage() is working !

--- app/src/main/java/com/Max/myapplication/Option.java
+++ app/src/main/java/com/Max/myapplication/Option.java
@@ -2,14 +2,56 @@ package com.Max.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.EditText;
+import android.widget.Spinner;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.androidnetworking.AndroidNetworking;
+import com.androidnetworking.error.ANError;
+import com.androidnetworking.interfaces.JSONObjectRequestListener;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 public class Option extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_option);
+        usage();
+    }
+
+    public void usage() {
+        String deeplKey = "2966ae25-76f9-e86d-94a3-8feac167f6f7:fx";
+
+        AndroidNetworking.get("https://api-free.deepl.com/v2/usage")
+                .addHeaders("Authorization", "DeepL-Auth-Key " + deeplKey)
+                .build()
+                .getAsJSONObject(new JSONObjectRequestListener() {
+                    @Override
+                    public void onResponse(JSONObject response) {
+                        try {
+                            //Récuperation du nombre de character
+                            String currentValue = String.valueOf(response.getInt("character_count"));
+                            String limitValue = String.valueOf(response.getInt("character_limit"));
+
+                            TextView counter = findViewById(R.id.counter);
+                            counter.setText(currentValue + " / " + limitValue);
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+                    }
+
+                    @Override
+                    public void onError(ANError anError) {
+                        Toast toast = Toast.makeText(Option.this, "Erreur", Toast.LENGTH_SHORT);
+                        toast.show();
+                    }
+                });
     }
 }
